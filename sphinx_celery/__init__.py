60da5509afb7bae5b1878b6dd027be0cdf68e2c5
Ask Solem <ask@celeryproject.org>
2016-07-01 14:03:12 -0700

    Adds bumpversion

--- sphinx_celery/__init__.py
+++ sphinx_celery/__init__.py
@@ -2,16 +2,11 @@
 from __future__ import absolute_import, unicode_literals
 
 import os
+import re
 
 from collections import namedtuple
 
-version_info_t = namedtuple(
-    'version_info_t', ('major', 'minor', 'micro', 'releaselevel', 'serial'),
-)
-
-VERSION = version_info = version_info_t(1, 3, 0, '', '')
-
-__version__ = '{0.major}.{0.minor}.{0.micro}{0.releaselevel}'.format(VERSION)
+__version__ = '1.3.0'
 __author__ = 'Ask Solem'
 __contact__ = 'ask@celeryproject.org'
 __homepage__ = 'http://github.com/celery/sphinx_celery'
@@ -20,6 +15,19 @@ __docformat__ = 'restructuredtext'
 # -eof meta-
 
 __all__ = ['get_html_templates_path', 'get_html_theme_path']
+
+version_info_t = namedtuple(
+    'version_info_t', ('major', 'minor', 'micro', 'releaselevel', 'serial'),
+)
+
+# bumpversion can only search for {current_version}
+# so we have to parse the version here.
+_temp = re.match(
+    r'(\d+)\.(\d+).(\d+)(.+)?', __version__).groups()
+VERSION = version_info = version_info_t(
+    int(_temp[0]), int(_temp[1]), int(_temp[2]), _temp[3] or '', '')
+del(_temp)
+del(re)
 
 
 def get_html_theme_path():
