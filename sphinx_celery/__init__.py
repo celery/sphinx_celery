73d1e106b59be62b1697d2a7807c8792a305b045
Omer Katz <omer.drow@gmail.com>
2019-02-13 13:55:41 +0200

    Bump version: 1.4.3 → 1.4.4

--- sphinx_celery/__init__.py
+++ sphinx_celery/__init__.py
@@ -6,7 +6,7 @@ import re
 
 from collections import namedtuple
 
-__version__ = '1.4.3'
+__version__ = '1.4.4'
 __author__ = 'Ask Solem'
 __contact__ = 'ask@celeryproject.org'
 __homepage__ = 'http://github.com/celery/sphinx_celery'
