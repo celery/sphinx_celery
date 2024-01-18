f73200d14ab3276b0e98ae5a9d2d61abb8838118
Christian Clauss <cclauss@me.com>
2024-01-18 14:47:52 +0100

    setup.py: Programming Language :: Python :: 3.12

--- setup.py
+++ setup.py
@@ -16,9 +16,11 @@ classes = """
     Development Status :: 5 - Production/Stable
     Programming Language :: Python
     Programming Language :: Python :: 3
-    Programming Language :: Python :: 3.7
     Programming Language :: Python :: 3.8
     Programming Language :: Python :: 3.9
+    Programming Language :: Python :: 3.10
+    Programming Language :: Python :: 3.11
+    Programming Language :: Python :: 3.12
     License :: OSI Approved :: BSD License
     Intended Audience :: Developers
     Operating System :: OS Independent
